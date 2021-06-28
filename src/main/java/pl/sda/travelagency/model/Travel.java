5bff3e89b1729e5c29e5943068f8766406102756
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-06-28 18:58:00 +0200

    db configuration

---
+++ src/main/java/pl/sda/travelagency/model/Travel.java
@@ -0,0 +1,32 @@
+package pl.sda.travelagency.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "TRAVEL")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Travel {
+
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date fromDate;
+
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date toDate;
+}
