09e74cb7f0f20b8e76c8d754353d630ab6f6184d
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-06 19:02:10 +0200

    front

--- src/main/java/pl/sda/travelagency/controller/MainController.java
+++ src/main/java/pl/sda/travelagency/controller/MainController.java
@@ -1,13 +1,35 @@
 package pl.sda.travelagency.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+    @GetMapping({"", "/index"})
     public String main() {
         return "index";
+    }
+
+    @GetMapping("/index-1")
+    public String page1() {
+        return "index-1";
+    }
+
+    @GetMapping("/index-2")
+    public String page2() {
+        return "index-2";
+    }
+
+    @GetMapping("/index-3")
+    public String page3() {
+        return "index-3";
+    }
+
+    @GetMapping("/index-4")
+    public String page4() {
+        return "index-4";
     }
 }
