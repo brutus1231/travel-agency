41ce913aaf5dab6bbab679d3b24dde1d8514e270
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-07 19:51:13 +0200

    logowanie

--- src/main/java/pl/sda/travelagency/TravelAgencyApplication.java
+++ src/main/java/pl/sda/travelagency/TravelAgencyApplication.java
@@ -1,16 +1,40 @@
 package pl.sda.travelagency;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import pl.sda.travelagency.config.SecurityConfiguration;
+import pl.sda.travelagency.model.RoleEntity;
+import pl.sda.travelagency.repository.RoleRepository;
 
 @SpringBootApplication
 @Import(SecurityConfiguration.class)
-public class TravelAgencyApplication {
+public class TravelAgencyApplication implements ApplicationRunner {
+
+    @Autowired
+    RoleRepository roleRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(TravelAgencyApplication.class, args);
     }
 
+
+    @Override
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+
+        RoleEntity userRole = roleRepository.findByName("USER");
+        if(userRole == null){
+            RoleEntity roleUser = new RoleEntity();
+            roleUser.setName("USER");
+            roleRepository.save(roleUser);
+
+            RoleEntity roleAdmin = new RoleEntity();
+            roleAdmin.setName("ADMIN");
+            roleRepository.save(roleAdmin);
+        }
+
+    }
 }
--- src/main/java/pl/sda/travelagency/model/RoleEntity.java
+++ src/main/java/pl/sda/travelagency/model/RoleEntity.java
@@ -1,9 +1,13 @@
 package pl.sda.travelagency.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "ROLE")
+@Getter @Setter
 public class RoleEntity {
 
     @Id
